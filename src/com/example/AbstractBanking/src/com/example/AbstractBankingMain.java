1e5156a52f9853735113459465ceda9126a2eae3
JayKay1508 <jayakumarece1989@gmail.com>
2016-12-28 11:52:33 +0530

    AbstractBanking

---
+++ src/com/example/AbstractBanking/src/com/example/AbstractBankingMain.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class AbstractBankingMain {
+//Create several customers and their accounts 
+	public static void main(String [] args){
+
+		Account a = new Customer(1508, "JaneSmith", "Employee");
+		a.getDescription();
+	}
+}
---
+++ src/com/example/AbstractBanking/src/com/example/TimeDepostiAccount.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class TimeDepostiAccount extends Account
+{	
+public TimeDepostiAccount(double balance)
+{
+	super(balance);
+}
+public void getDescription(){
+	
+}
+public Boolean withdraw(double amount)
+{
+	return true;
+}
+}
