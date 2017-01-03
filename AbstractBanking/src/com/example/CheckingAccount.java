b3db01e9f098e43d07036d4684b5d588fa89e90f
JayKay1508 <jayakumarece1989@gmail.com>
2017-01-03 12:10:42 +0530

    Abstract Class

---
+++ AbstractBanking/src/com/example/CheckingAccount.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class CheckingAccount extends Account 
+{	
+public CheckingAccount(double balance){
+	super(balance);
+}
+@Override
+public void getDescription()
+{
+	
+}
+@Override
+public Boolean withdraw(double amount)
+{
+	return null;
+	}
+}
+
